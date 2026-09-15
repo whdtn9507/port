e8aaae553b8884f9feb770ab391b08f7b7ff705e
whdtn9507 <whdtn9506@naver.com>
2026-09-15 23:03:58 +0900

    Initial commit

---
+++ 04(영화홈페이지)/js/drop.js
@@ -0,0 +1,85 @@
+function myFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
+function myFunction2(){
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+function myFunction3(){
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
+
+function myFunction4(){
+    document.getElementById("myDropdown4").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function myFunction5(){
+    document.getElementById("myDropdown5").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
